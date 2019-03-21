836536cf4eee9be349d0c69773143b92be6af3df
Eric Zhou <qqzhoucn@gmail.com>
2019-03-22 05:32:35 +0800

    sort csv

---
+++ csv_merge.py
@@ -0,0 +1,62 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import urllib.request
+import certifi
+#import ocr
+import ocr_ut
+import cvcrop
+import blob
+import get_peg
+import csv_splitter
+import pandas as pd
+import os
+import glob
+from multiprocessing import Pool
+
+def f(x):
+    return x*x
+
+p = Pool(5)
+print(p.map(f, [1, 2, 3]))
+
+# Function definition is here
+def printme( str ):
+   "This prints a passed string into this function"
+   print (str)
+   return;
+
+path = './'
+extension = 'csv'
+os.chdir(path)
+result = [i for i in glob.glob('peg_output*.{}'.format(extension))]
+#for x in result:
+#    print(x)
+    #get_peg_from_csv(x)
+
+print('=======')
+
+csv_file = 'out.csv'
+
+fout=open(csv_file,"a")
+# first file:
+for line in open("peg_output_1.csv.csv"):
+    fout.write(line)
+# now the rest:    
+for x in result:
+    print(x)
+    if(x == 'peg_output_1.csv.csv'):
+        continue
+
+    f = open(x)
+    next(f) # skip the header
+    for line in f:
+         fout.write(line)
+    f.close() # not really needed
+fout.close()
+
+
+
---
+++ csv_sort.py
@@ -0,0 +1,55 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import urllib.request
+import certifi
+#import ocr
+import ocr_ut
+import cvcrop
+import blob
+import get_peg
+import csv_splitter
+import pandas as pd
+import os
+import glob
+import re
+from multiprocessing import Pool
+
+
+# Function definition is here
+def printme( str ):
+   "This prints a passed string into this function"
+   print (str)
+   return;
+
+def csv_sort( filename ):
+    csv_file = filename
+    df = pd.read_csv(csv_file)
+    #print(df)
+    print('=======')
+    print(df.shape)
+    data_list = []
+    for i in range(df.shape[0]):
+        #print(df.iloc[i,0])
+        #process = "{:.2f}".format( 100.0 * i / df.shape[0] )
+        #print(process)
+        #print('i / total = ' + str(i) + ' / ' + str(df.shape[0]))
+        line = df.iloc[i,0]
+        ss = re.findall(r'\S+', line)
+        #print(ss)
+        #peg = 0.0
+        #s2 = "{:.2f}".format( peg ) # new
+        data_list.append([ss[1],float(ss[2])])
+    df = pd.DataFrame(data_list,columns=['Code','PEG'])
+    final_df = df.sort_values(by='PEG')
+    print(final_df)
+    final_df.to_csv('1_peg_' + filename + '.csv', sep='\t', encoding='utf-8')
+    return;
+
+csv_file = 'out.csv'
+csv_sort(csv_file)
+
+
